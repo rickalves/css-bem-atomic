fe1296a6a5eaf39e6d603c59838ea7c46d636878
Rick_Alves <henryworkdev.ti@gmail.com>
2022-03-14 01:47:07 -0300

    featura/desktop-design

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const menu_lateral = document.querySelector(".menu-lateral")
 const btn_menu = document.querySelector(".cabecalho__menu")
 
-
 btn_menu.addEventListener('click', (e) => {
     menu_lateral.classList.toggle('menu-lateral--visivel')
 })
